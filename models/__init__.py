8feab9be2885d09cc1331d7dac4f809ee9b4e5fc
ZamaZoe <zamarzoe57@gmail.com>
2023-01-12 13:37:51 +0200

    Initial Commit

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+from models.store import StoreModel
+from models.item import ItemModel
+from models.tag import TagModel
+from models.item_tags import ItemsTags
+from models.user import UserModel
+
+
+
+
+
---
+++ resources/store.py
@@ -0,0 +1,51 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+from models import StoreModel
+from db import db
+from schemas import StoreSchema
+
+
+blp = Blueprint("stores", __name__, description="Operations on stores")
+
+
+@blp.route("/store/<int:store_id>")
+class Store(MethodView):
+    @blp.response(200, StoreSchema)
+    def get(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        return store
+
+    def delete(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted"}, 200
+    
+            
+            
+@blp.route("/store")
+class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
+    def get(cls):
+        return StoreModel.query.all()
+    
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
+    def post(cls, store_data):
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                message="A store with that name already exists.",
+                )
+        except SQLAlchemyError:
+            abort(500, message="An error occurred creating the store.")
+
+        return store
